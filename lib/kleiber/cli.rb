59b0ed4ff3c05ec7f90a7b2a08433b7f8b6dd5a0
kirill_bobykin <qelphybox@gmail.com>
2016-09-01 00:13:31 +0300

    Added docs for symphony

--- lib/kleiber/cli.rb
+++ lib/kleiber/cli.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+
 require 'thor'
 require_relative 'symphony'
 
@@ -9,32 +10,32 @@ module Kleiber
 
     desc 'up', 'send vagrant up to machines'
     def up(*projects)
-      Symphony.new('up', options: options, projects: projects) #.exec
+      Symphony.new('up', options: options, projects: projects).perform
     end
 
     desc 'ssh', 'send vagrant ssh to machines'
     def ssh(*projects)
-      Symphony.new('ssh', options: options, projects: projects) #.exec
+      Symphony.new('ssh', options: options, projects: projects).perform
     end
 
     desc 'halt', 'send vagrant halt to machines'
     def halt(*projects)
-      Symphony.new('halt', options: options, projects: projects) #.exec
+      Symphony.new('halt', options: options, projects: projects).perform
     end
 
     desc 'reload', 'send vagrant reload to machines'
     def reload(*projects)
-      Symphony.new('reload', options: options, projects: projects) #.exec
+      Symphony.new('reload', options: options, projects: projects).perform
     end
 
     desc 'destroy', 'send vagrant destroy to machines'
     def destroy(*projects)
-      Symphony.new('destroy', options: options, projects: projects) #.exec
+      Symphony.new('destroy', options: options, projects: projects).perform
     end
 
     desc 'provision', 'send provision vagrant to machines'
     def provision(*projects)
-      Symphony.new('provision', options: options, projects: projects) #.exec
+      Symphony.new('provision', options: options, projects: projects).perform
     end
   end
 end
--- lib/kleiber/symphony.rb
+++ lib/kleiber/symphony.rb
@@ -5,17 +5,32 @@ require 'json'
 require_relative 'project'
 
 module Kleiber
+  # Model describes job which you want to perform.
+  # Roughly speaking, this is event of command performing on defined Vagrant machines.
+  # Looks like symphony of vagrants.
+  # @author Bobykin Kirill <qelphybox@gmail.com>
   class Symphony
+    # Creates an instance of symphony
+    # @param command [Symbol] Vagrant command to perform
+    # @param args = {} [Hash] arguments for perfomance
+    # @option params [Hash] options
+    # @option params [Array] projects ([]) list of projects
+    # @return [Symphony] instance of Symphony
     def initialize(command, args = {})
       options = args[:options]
+      projects_by_config = Project.load_by_config(options[:config])
+      @option = options[:option]
       @command = command
-      @projects = Project.load_by_config(options[:config])
+      @projects = if args[:projects].empty?
+                    projects_by_config
+                  else
+                    projects_by_config.select { |p| args[:projects].include?(p.name) }
+                  end
     end
 
-    private
-
-    def load_config(config_path)
-
+    # Perform created symphony
+    def perform
+      # TODO: implement perfomance
     end
   end
 end
