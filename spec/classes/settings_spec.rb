1e4ba5de8881a473e53de5ba5ad9965d18086ef6
Bobykin Kirill <qelphybox@gmail.com>
2016-12-05 21:24:28 +0300

    Added simple tests for settings

--- spec/classes/settings_spec.rb
+++ spec/classes/settings_spec.rb
@@ -3,10 +3,10 @@
 RSpec.describe Kleiber::Settings do
   context 'instance methods' do
     subject(:settings) { Kleiber::Settings.new(path) }
+    let(:path) { good_config_path }
     context '#valid?' do
       subject { settings.valid? }
       context 'good config' do
-        let(:path) { good_config_path }
         it { is_expected.to eq true }
       end
 
@@ -14,6 +14,21 @@ RSpec.describe Kleiber::Settings do
         let(:path) { bad_config_path }
         it { is_expected.to eq false }
       end
+    end
+
+    context '#projects' do
+      subject { settings.projects }
+      it { is_expected.to be_an_instance_of(Array) }
+    end
+
+    context '#tasks' do
+      subject { settings.tasks }
+      it { is_expected.to be_an_instance_of(Hash) }
+    end
+
+    context '#storages_path' do
+      subject { settings.storages_path }
+      it { is_expected.to be_an_instance_of(String) }
     end
   end
 end
