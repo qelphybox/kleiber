cfa9cb8064086d3bfd7dc4d15fd2582065ab57b1
Bobykin Kirill <qelphybox@gmail.com>
2016-12-05 21:33:27 +0300

    Added simple tests for kleiber

--- spec/kleiber_spec.rb
+++ spec/kleiber_spec.rb
@@ -1,8 +1,23 @@
 # encoding: utf-8
 
 RSpec.describe Kleiber do
+  include_context 'good config'
   context '#projects' do
     subject { Kleiber.projects }
     it { is_expected.to be_an_instance_of(Array) }
+    it 'is array of projects' do
+      is_expected.to satisfy do |result|
+        result.all? { |p| p.is_a?(Kleiber::Project) }
+      end
+    end
+  end
+
+  context '#configure' do
+    specify { expect { |b| Kleiber.configure(&b) }.to yield_with_args(an_instance_of(Kleiber::Settings)) }
+  end
+
+  context '#settings' do
+    subject { Kleiber.settings }
+    it { is_expected.to be_an_instance_of(Kleiber::Settings) }
   end
 end
