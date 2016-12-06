e1de817b37384e2ecc92b798137c81a2ae80d240
Bobykin Kirill <qelphybox@gmail.com>
2016-12-07 00:10:36 +0300

    Added gem cocaine and Kleiber::Commands module

---
+++ lib/kleiber/commands.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+
+require 'cocaine'
+
+module Kleiber
+  module Commands
+    def vagrant
+      Cocaine::CommandLine.new('unset RUBYLIB;', '/usr/bin/vagrant')
+    end
+
+    # Returns vagrant up command line
+    # @return [Cocaine::CommandLine] vagrant up command
+    def vagrant_up
+      Cocaine::CommandLine.new(vagrant.command, 'up')
+    end
+
+    # Returns vagrant ssh command line
+    # @return [Cocaine::CommandLine] vagrant ssh command
+    def vagrant_ssh(_command)
+      Cocaine::CommandLine.new(vagrant.command, 'ssh')
+    end
+
+    # Returns vagrant halt command line
+    # @return [Cocaine::CommandLine] vagrant halt command
+    def vagrant_halt
+      Cocaine::CommandLine.new(vagrant.command, 'halt')
+    end
+
+    # Returns vagrant destroy command line
+    # @return [Cocaine::CommandLine] vagrant destroy command
+    def vagrant_destroy
+      Cocaine::CommandLine.new(vagrant.command, 'destroy')
+    end
+
+    # Returns vagrant provision command line
+    # @return [Cocaine::CommandLine] vagrant provision command
+    def vagrant_provision
+      Cocaine::CommandLine.new(vagrant.command, 'provision')
+    end
+  end
+end
